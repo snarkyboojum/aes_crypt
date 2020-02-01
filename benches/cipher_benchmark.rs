9ce942b7cd840241f7b4f8bbe468e51f5a2c0400
Adrian White <snarkyboojum@gmail.com>
2020-02-01 15:59:22 +1100

    Adding throughput benchmarks

--- benches/cipher_benchmark.rs
+++ benches/cipher_benchmark.rs
@@ -1,7 +1,7 @@
 use aes_crypt::{cipher, expand_key, KeyLength};
-use criterion::{criterion_group, criterion_main, Criterion};
+use criterion::{criterion_group, criterion_main, Criterion, Throughput};
 
-fn criterion_benchmark(c: &mut Criterion) {
+fn cipher_latency(c: &mut Criterion) {
     let plaintext: [u8; 16] = [
         0x00, 0x11, 0x22, 0x33, 0x44, 0x55, 0x66, 0x77, 0x88, 0x99, 0xaa, 0xbb, 0xcc, 0xdd, 0xee,
         0xff,
@@ -24,5 +24,29 @@ fn criterion_benchmark(c: &mut Criterion) {
     });
 }
 
-criterion_group!(benches, criterion_benchmark);
+fn cipher_throughput(c: &mut Criterion) {
+    let plaintext: [u8; 16] = [
+        0x00, 0x11, 0x22, 0x33, 0x44, 0x55, 0x66, 0x77, 0x88, 0x99, 0xaa, 0xbb, 0xcc, 0xdd, 0xee,
+        0xff,
+    ];
+    let key: [u8; 16] = [
+        0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0a, 0x0b, 0x0c, 0x0d, 0x0e,
+        0x0f,
+    ];
+    // expand the key once
+    let expanded_key = &mut [0u32; 44];
+    expand_key(&key, expanded_key, KeyLength::OneTwentyEight);
+
+    let mut group = c.benchmark_group("cipher_throughput");
+    group.throughput(Throughput::Bytes(plaintext.len() as u64));
+    group.bench_function("cipher test_data", |b| {
+        b.iter(|| {
+            let _output: [u8; 16] = cipher(&plaintext, expanded_key);
+        })
+    });
+    group.finish();
+}
+
+//criterion_group!(benches, cipher_latency);
+criterion_group!(benches, cipher_latency, cipher_throughput);
 criterion_main!(benches);
